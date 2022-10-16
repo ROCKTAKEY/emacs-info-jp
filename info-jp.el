ba534bb836f32f2407f3302d894003fb7224c6dd
ROCKTAKEY <rocktakey@gmail.com>
2022-10-16 18:17:44 +0900

    Initial commit

---
+++ info-jp.el
@@ -0,0 +1,38 @@
+;;; info-jp.el --- Provide info files of Emacs and Emacs Lisp reference translated to Japanese  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  ROCKTAKEY
+
+;; Author: ROCKTAKEY <rocktakey@gmail.com>
+;; Keywords: help
+
+;; Version: 0.0.0
+;; Package-Requires: ((emacs "24.1"))
+;; URL: https://github.com/ROCKTAKEY/emacs-info-jp
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; Provide info files of Emacs and Emacs Lisp reference translated to Japanese
+
+;;; Code:
+
+(defgroup info-jp ()
+  "Provide info files of Emacs and Emacs Lisp reference translated to Japanese"
+  :group 'help
+  :prefix "info-jp-"
+  :link '(url-link "https://github.com/ROCKTAKEY/emacs-info-jp"))
+
+(provide 'info-jp)
+;;; info-jp.el ends here
